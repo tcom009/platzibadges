189d2b922dd36a9f424d838e0b84000bfa9e1bd6
Gerson Mayer <g.mayerduran@gmail.com>
2019-08-02 14:26:01 -0500

    Clase1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+
+// // esto es sin usar react
+// //creamos un objeto tipo h1
+// const element = document.createElement ('h1');
+// //añadimos texto al objeto
+// element.innerText = 'Hello, Platzi Badges';
+
+// //instanciamos un elemento "contenedor" al que le vamos a pasar nuestro elemento h1
+// const container = document.getElementById ('app');
+
+// //llamamos a nuetro container y le pasamos nuestro "element"
+// container.appendChild(element);
+//React es el analogo a createElement, es necesario siempre que usemos JXS
+import React from  'react';
+//ReactDOM es el analogo de appendChild
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//el siguiente h1 es JSX 
+const element = <h1> Hello, Platzi Badges, from react</h1>;
+const container = document.getElementById ('app')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render (que vamos a renderizar, donde lo vamos a renderizar)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render  (element, container);
+
+
