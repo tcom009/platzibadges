74d389071fe5f3215c2e8f9602eeae58825766fe
Gerson Mayer <g.mayerduran@gmail.com>
2020-01-01 20:05:35 -0500

    se termino la pagina home y se reviso la clase 21

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,19 @@ import Layout from './Layout'
 import BadgeNew from '../pages/BadgeNew';
 import Badges from '../pages/Badges';
 import NotFound from '../pages/NotFound'
-
+import Home from '../pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
     <Layout>
       <Switch>
+        <Route exact path="/" component = {Home} />
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
         <Route component = {NotFound} />
+
+
       </Switch>
     </Layout>
     </BrowserRouter>
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import astronauts from '../images/astronauts.svg'
+import stars from '../images/stars.svg'
+import platziLogo from '../images/platziconf-logo.svg'
+import './styles/Home.css'
+import {Link} from 'react-router-dom'
+
+
+function Home  () {
+return (
+    <React.Fragment>
+    <div className= "Home">
+    <div className= "container">  
+    <div className = "row">
+
+            <div className = "Home__col col-12 col-md-4">
+            <img src = {platziLogo}/>
+            
+            <br /> 
+            <h1 >
+                Imprime tus 
+            credenciales
+            </h1>
+            <Link to ="/badges" className="btn btn-primary"> Iniciar</Link>
+            </div>
+            <div className= "Home__col col-12 col-md-4">    
+            <img src = {astronauts}/>
+            </div>
+    </div>
+    </div>
+    </div>
+
+    </React.Fragment>
+
+)
+
+}
+
+export default Home 
